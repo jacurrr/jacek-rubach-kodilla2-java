e844d6575ae1eeb8e1a59637973b3a65e3abccd6
jacurrr <jacek.rubach@gmail.com>
2020-09-07 18:42:52 +0200

    Moduł 30.3 Zadanie

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -27,4 +27,26 @@ public class StoredProcTestSuite {
         }
         Assert.assertEquals(0,howMany);
     }
+
+    @Test
+    public void testUpdateBestsellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE books SET BESTSELLER=false ";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.execute(sqlUpdate);
+
+        //When
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM books WHERE BESTSELLER=true";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        Assert.assertEquals(1, howMany);
+    }
 }
