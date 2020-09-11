2bd29fab97262c99901269bf96f1ae941fae3da2
jacurrr <jacek.rubach@gmail.com>
2020-09-11 17:08:06 +0200

    Moduł 31.2 Teoria

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -0,0 +1,18 @@
+package com.kodilla.testing2.google;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GoogleTestingApp {
+    public static final String SEARCHFIELD = ".gLFyf.gsfi";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.google.com");
+        WebElement searchField = driver.findElement(By.cssSelector(SEARCHFIELD));
+        searchField.sendKeys("Kodilla");
+        searchField.submit();
+    }
+}
